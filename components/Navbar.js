d9a827d1a1bc311be0f50bfa0c246eb03a13ca76
christyanmst <christyan228@gmail.com>
2022-10-27 11:13:22 -0300

    Estruturando form

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+function Navbar(){
+
+    return(
+        <>
+            <nav className={"navbar"}>
+                <h3>SITE EM CONSTRUÇÃO</h3>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { getCities, getCountries } from "./api/config";
-import { maskCPF, maskPhone } from "./masks";
+import { maskCPF, maskPhone } from "../components/Masks";
 
 export default function Home() {
   const [citiesList, setCitiesList] = useState([]);
@@ -10,14 +10,13 @@ export default function Home() {
 
   useEffect(() => {
     getCountries()
-      .then((res) => setCountriesList(res.data.map(e => e.name_ptbr)))
+      .then((res) => setCountriesList(res.data.map((e) => e.name_ptbr)))
       .catch((err) => console.log(err));
     getCities()
-      .then((res) => setCitiesList(res.data.map(e => e.name_ptbr)))
+      .then((res) => setCitiesList(res.data.map((e) => e.name_ptbr)))
       .catch((err) => console.log(err));
   }, []);
 
-console.log
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -27,36 +26,71 @@ console.log
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>Nome</label>
-        <input type="text" name="name" placeholder="Nome" />
-        <label>Email</label>
-        <input type="email" name="email" placeholder="seuemail@email.com" />
-        <label>Telefone</label>
-        <input type="text" name="phone" value={phone} onChange={e => setPhone(maskPhone(e.target.value))} placeholder="(00) 00000-0000" />
-        <label>CPF</label>
-        <input type="text" name="cpf" value={cpf} onChange={e => setCPF(maskCPF(e.target.value))} placeholder="000.000.000-00" />
-        <label>Países</label>
-        <select name="country">
-          <option value="">Selecione...</option>
-          {countriesList.map((res, index) => (
-            <option key={index} value={res}>
-              {res}
-            </option>
-          ))}
-        </select>
-        <label>Cidades</label>
-        <select name="city">
-          <option value="">Selecione...</option>
-          {citiesList.map((res, index) => (
-            <option key={index} value={res}>
-              {res}
-            </option>
-          ))}
-        </select>
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
+    <>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Nome</label>
+            <input type="text" name="name" placeholder="Nome" required />
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="seuemail@email.com"
+              required
+            />
+          </div>
+          <div>
+            <label>Telefone</label>
+            <input
+              type="text"
+              name="phone"
+              value={phone}
+              onChange={(e) => setPhone(maskPhone(e.target.value))}
+              placeholder="(00) 00000-0000"
+              required
+            />
+          </div>
+          <div>
+            <label>
+              CPF
+              <input
+                type="text"
+                name="cpf"
+                value={cpf}
+                onChange={(e) => setCPF(maskCPF(e.target.value))}
+                placeholder="000.000.000-00"
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>Países</label>
+            <select name="country" style={{ width: "150px" }} required>
+              <option value="">Selecione...</option>
+              {countriesList.map((res, index) => (
+                <option key={index} value={res}>
+                  {res}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label>Cidades</label>
+            <select name="city" style={{ width: "150px" }} required>
+              <option value="">Selecione...</option>
+              {citiesList.map((res, index) => (
+                <option key={index} value={res}>
+                  {res}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </>
   );
 }
